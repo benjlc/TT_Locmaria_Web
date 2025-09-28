266c1ebc0b724b322693b06a161a5e231e6d3ca3
benjlc <benjaminlecorre2006@gmail.com>
2025-09-29 00:35:35 +0200

    Ton message de commit

--- js/script.js
+++ js/script.js
@@ -5,3 +5,16 @@ menuToggle.addEventListener('click', () => {
   navLinks.classList.toggle('show');
 });
 
+const lightbox = document.getElementById('lightbox');
+const lightboxImg = lightbox.querySelector('img');
+
+document.querySelectorAll('.clickable-affiche').forEach(img => {
+  img.addEventListener('click', () => {
+    lightboxImg.src = img.src;
+    lightbox.style.display = 'flex';
+  });
+});
+
+lightbox.addEventListener('click', () => {
+  lightbox.style.display = 'none';
+});
