789c9f62705085373aa7bbbd6d8b607e35a6360a
benjlc <benjaminlecorre2006@gmail.com>
2025-10-05 12:46:46 +0200

    Message de commit

---
+++ v1/js/script.js
@@ -0,0 +1,20 @@
+const menuToggle = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+menuToggle.addEventListener('click', () => {
+  navLinks.classList.toggle('show');
+});
+
+const lightbox = document.getElementById('lightbox');
+const lightboxImg = lightbox.querySelector('img');
+
+document.querySelectorAll('.clickable-affiche').forEach(img => {
+  img.addEventListener('click', () => {
+    lightboxImg.src = img.src;
+    lightbox.style.display = 'flex';
+  });
+});
+
+lightbox.addEventListener('click', () => {
+  lightbox.style.display = 'none';
+});
